fca8b06acefc41f607fe92b38b859f9e3c4aac41
Bernard Tatin <bernard.tatin@outlook.fr>
2019-04-25 12:53:09 +0200

    ajout de mk-bash et mk-zsh

---
+++ bin/mk-bash.bash
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+script=$(basename $0)
+script_dir=$(dirname $0)
+
+dohelp() {
+	local exit_code=0
+
+	[ $# -gt 0 ] && exit_code=$1 && shift
+	[ $# -gt 0 ] && echo "ERROR: $*" 1>&2
+	
+	cat >> DOHELP
+${script} [-h|--help] : this message
+${script} name name ... : shell script names
+DOHELP
+	exit ${exit_code}
+}
+
+make_sh() {
+	cat > $1 << MAKE_SH
+#!/bin/sh
+
+script=\$(basename \$0)
+script_dir=\$(dirname \$0)
+
+dohelp() {
+	exit_code=0
+	
+	[ \$# -gt 0 ] && exit_code=\$1 && shift
+	[ \$# -gt 0 ] && echo "ERROR: \$*" 1>&2
+	
+	cat >> DOHELP
+\${script} [-h|--help] : this message
+\${script} name name ... : shell script names
+DOHELP
+	exit \${exit_code}
+}
+
+while [ \$# -ne 0 ]
+do
+	case \$1 in
+		-h|--help)
+			dohelp
+			;;
+		*)
+			;;
+	esac
+	shift
+done
+MAKE_SH
+	chmod +x $1
+}
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+		-h|--help)
+			dohelp
+			;;
+		*)
+			make_sh $1
+			;;
+	esac
+	shift
+done
+
