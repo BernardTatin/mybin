4bacf951ba3588264da0e4ca4d6a94722a4baa3c
Bernard Tatin <bernard.tatin@outlook.fr>
2019-05-05 02:01:10 +0200

    Book creation works well!

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+---
+author: Bernard Tatin
+title: Histoires de shells
+
+---
+
+---
 # README
 
-Beaucoup de notes à propos des shells et de l'administration _Linux_.
+Beaucoup de notes à propos des shells et de l'administration _Linux_. Et des scripts, beaucoup de scripts.
--- bin/dodoc.sh
+++ bin/dodoc.sh
@@ -1,12 +1,17 @@
-#!/bin/sh 
+#!/bin/sh
 
 . $(dirname $0)/base.inc.sh
 
 startdir=.
+mdfiles=
 docdir=./fulldoc
 css=$(dirname ${here})/docstyles/default.css
 doclear=0
+makebook=0
+tmpmd=/tmp/mypandoc.md
 debug=0
+
+rm -f ${tmpmd}
 
 dohelp() {
     ecode=0
@@ -18,7 +23,7 @@ dohelp() {
     fi
     cat << DOHELP
 ${script} -h|--help: this text
-${script} [OPTIONS]: convert all markdown files of current dir to PDF
+${script} [OPTIONS] [FILES.md]: convert all markdown files of current dir to PDF
 OPTIONS:
     --startdir dir: use dir instead of current dir, default ${startdir}
     --docdir dir: destination dir of PDF files, default ${docdir}
@@ -30,12 +35,15 @@ DOHELP
 
 show_config() {
     [ ${debug} -eq 1 ] && \
+        echo '============================================='
         echo "here:         $here" && \
         echo "script:       $script" && \
         echo "startdir:     $startdir" && \
         echo "docdir:       $docdir" && \
         echo "css:          $css" && \
-        echo "doclear:      $doclear"
+        echo "doclear:      $doclear" && \
+        echo "mdfiles:      $mdfiles" && \
+        echo '============================================='
 }
 
 while [ $# -ne 0 ]
@@ -48,17 +56,17 @@ do
             doclear=1
             ;;
         --startdir)
-            shift 
+            shift
             [ $# -eq 0 ] && dohelp ${FAILURE} "$1 must be followed by a dir name"
             startdir="$1"
             ;;
         --css)
-            shift 
+            shift
             [ $# -eq 0 ] && dohelp ${FAILURE} "$1 must be followed by a css name"
             css="$1"
             ;;
         --docdir)
-            shift 
+            shift
             [ $# -eq 0 ] && dohelp ${FAILURE} "$1 must be followed by a dir name"
             docdir="$1"
             ;;
@@ -66,34 +74,42 @@ do
             debug=1
             ;;
         *)
-            dohelp ${FAILURE} "unknown option '$1'"
+            mdfiles=${tmpmd}
+            while [ $# -gt 0 ]; do
+              cat $1 >> ${tmpmd}
+              echo >> ${tmpmd}
+              shift
+            done
             ;;
     esac
-    shift
+    [ $# -gt 0 ] && shift
 done
 
-show_config 
+show_config
 
-startdir=$(standardize_dir $startdir)
+! [ -n "${mdfiles}" ] \
+    && startdir=$(standardize_dir $startdir) \
+    && mdfiles=$(cd ${startdir} && find . -name '*.md')
 mkdir -p $docdir
 docdir=$(standardize_dir $docdir)
 
-show_config 
+show_config
 
 [ ${doclear} -eq 1 ] && rm -rf ${docdir}
 
 cd ${startdir}
 
-for f in $(find . -name '*.md')
+for f in ${mdfiles}
 do
     echo "$f ...."
     fdir=$(dirname $f)
     fname=$(basename $f .md)
     ddir=$(echo $fdir | sed "s!^\.!${docdir}!")
     echo "$f -> $ddir/$fname.pdf"
-    mkdir -p $ddir 
+    mkdir -p $ddir
     [ ${debug} -ne 1 ] && \
         pandoc --standalone \
+            --toc \
             --css ${css} \
             -f markdown \
             -t html \
