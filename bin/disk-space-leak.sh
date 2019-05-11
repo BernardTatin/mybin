85e0c279026618622679c3bee06bbd29e19ba0b1
Bernard Tatin <bernard.tatin@outlook.fr>
2019-05-11 22:35:51 +0200

    Extension de la gestion d'erreur et de tout le reste à disk-space-leak.sh

--- bin/disk-space-leak.sh
+++ bin/disk-space-leak.sh
@@ -1,35 +1,53 @@
-#!/usr/bin/env bash
+#!/usr/bin/env dash
 
 # show the first 3 directories/files which
 # occupie the most disk space
 # usage:
 #
 
-# set -x
-# it is an error to read an uninitialized variable
-set -o nounset
-# Causes a pipeline to return the exit status of the last command
-# in the pipe that returned a non-zero return value.
-# set -o pipefail
-## the name of the script
-script=$(basename $0)
+# ----------------------------------------------------------------------
+set -e
+set -u
+
+. $(dirname $0)/base.inc.sh
+
+# variables
+retcode=${retcode:-$FAILURE}
+
+# ----------------------------------------------------------------------
+trap_exit() {
+  # echo "trap_exit ${retcode}"
+  exit ${retcode}
+}
+trap_error() {
+  retcode=${FAILURE}
+  echo "trap_error ${retcode}"
+}
+trap_force_quit() {
+  retcode=${FAILURE}
+  echo "trap_force_quit ${retcode}"
+}
+# ----------------------------------------------------------------------
+# trap trap_force_quit *
+trap trap_force_quit TERM TSTP INT QUIT
+trap trap_error HUP ILL ABRT FPE SEGV PIPE
+trap trap_exit EXIT
 
 # the du command with parameters:
 # -s : summarize each elements
 # -m : values in megabytes
 _du="du -sm"
 
-function dohelp() {
+get_help_text() {
    cat <<HELP
 $script --help: this text
 $script: as $script *
 $script dir dir ...: show the 3 biggest directories/files
 HELP
-   exit
 }
 
-[[ $# -gt 0 ]] \
-   && [[ $1 = "--help" ]] \
+[ $# -gt 0 ] \
+   && [ $1 = "--help" ] \
    && dohelp
 
 case $# in
@@ -43,3 +61,5 @@ case $# in
       ${_du} "$@"
       ;;
 esac | sort -nr | head -3
+
+retcode=$SUCCESS
